ce8a0b762aca1727c370068c12dd347865dd2ab3
Neo Sock Cheng <116181300+NeoSockCheng@users.noreply.github.com>
2025-06-07 19:35:57 +0800

    Add speech recognition module (#1)
    * added speech recognition test files
    
    * topic update
    
    * add gemini text processing
    
    * update speech recognition module
    
    * remove gitignore files
    
    * update speech recognition module
    
    * make user input more flexible

---
+++ scripts/google_sr.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+import speech_recognition as sr
+import sounddevice
+
+def googlesr():
+    rospy.init_node('google_sr', anonymous=True)
+    rospy.Subscriber("item_finder_sr_termination", String, callback)
+    pub = rospy.Publisher('item_finder_input', String, queue_size=10)
+
+    while not rospy.is_shutdown():
+        # obtain audio from the microphone
+        r = sr.Recognizer()
+        
+        with sr.Microphone(device_index=1) as source:
+            # print(">>> Tell me what you want to find!")
+            r.adjust_for_ambient_noise(source)
+            audio = r.listen(source)
+            # audio = r.record(source, duration=5)
+            
+        # recognize speech using Google Speech Recognition
+        try:
+            result = "I heard you are saying: " + r.recognize_google(audio, language="en-US")
+            print(result)
+            pub.publish(result)
+        except sr.UnknownValueError:
+            result = "I could not understand your audio, please try again."
+            print(result)
+        except sr.RequestError as e:
+            result = "Could not request results from Google Speech Recognition service."
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
+            
+def callback(msg):
+    if msg=="stop":
+        rospy.loginfo("[Speech Listener] Object extraction complete. Stopping speech recognition...")
+        rospy.signal_shutdown("Object extraction complete.")
+
+if __name__ == "__main__":
+    try:
+        print(f"Available mics: {sr.Microphone.list_microphone_names()}")
+        # rospy.loginfo(f"Available mics: {sr.Microphone.list_microphone_names()}")
+        googlesr()
+    except rospy.ROSInterruptException:
+        pass
