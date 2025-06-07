aac52387c521a98f8727723f4ed3f44b883f2e8a
Neo <sockchengneo@gmail.com>
2025-06-07 14:56:07 +0800

    added speech recognition test files

---
+++ scripts/speech_listener.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+
+def callback(msg):
+    rospy.loginfo("Heard: %s", msg.data)
+
+def speech_listener():
+    rospy.init_node('speech_listener', anonymous=True)
+    rospy.Subscriber("result", String, callback)
+    rospy.loginfo("Speech listener is running. Waiting for speech...")
+    rospy.spin()
+
+if __name__ == "__main__":
+    try:
+        speech_listener()
+    except rospy.ROSInterruptException:
+        pass
